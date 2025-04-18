ccc0d0b423e6c5e6c32514be7b3985f794b47659
Jasiek <jasiekk9@gmail.com>
2025-04-18 17:37:38 +0200

    Update __init__ files for players and core

---
+++ core/__init__.py
@@ -0,0 +1,7 @@
+from core.game_env import GameEnv
+from core.game_generator import GameGenerator
+from core.match_manager import MatchManager
+
+__all__ = [
+    'GameEnv', 'GameGenerator', 'MatchManager'
+]
--- players/__init__.py
+++ players/__init__.py
@@ -1,4 +1,6 @@
 from players.base_player import BasePlayer
 from players.external_player import ExternalPlayer
+from players.bogo_player import BogoPlayer
+from players.othello_heuristics import OthelloPositionalPlayer, OthelloMobilityPlayer
 
-__all__ = ['ExternalPlayer']
+__all__ = ['ExternalPlayer', 'BogoPlayer', 'OthelloPositionalPlayer', 'OthelloMobilityPlayer']
