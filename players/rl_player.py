6aadb968a16e6d3c5aba0633a06f92a1930f4b31
Jasiek <jasiekk9@gmail.com>
2025-04-18 19:44:02 +0200

    Add `deterministic` flag to RLPlayer

--- players/rl_player.py
+++ players/rl_player.py
@@ -6,11 +6,12 @@ from players.base_player import BasePlayer
 
 class RLPlayer(BasePlayer):
 
-    def __init__(self, model: BaseAlgorithm):
+    def __init__(self, model: BaseAlgorithm, deterministic: bool = False):
         super().__init__()
         self.model = model
+        self.deterministic = deterministic
 
     def get_move(self, game: BaseGame) -> BaseMove:
         action_masks = game.action_masks()
-        action, _ = self.model.predict(game.get_obs("flat"), action_masks=action_masks)
+        action, _ = self.model.predict(game.get_obs("flat"), action_masks=action_masks, deterministic=self.deterministic)
         return game.get_move_from_action(action)
