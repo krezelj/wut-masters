3a642f570085ccc33cd0c3fa46703f8b78c356d2
Jasiek <jasiekk9@gmail.com>
2025-04-30 14:15:09 +0200

    Change CNN architecture of Othello Feature Extractor

--- architectures/othello_cnn.py
+++ architectures/othello_cnn.py
@@ -17,11 +17,13 @@ class SimpleCNN(BaseFeaturesExtractor):
         super().__init__(observation_space, features_dim)
         n_input_channels = observation_space.shape[0]
         self.cnn = nn.Sequential(
-            nn.Conv2d(n_input_channels, 32, kernel_size=2, stride=1, padding=0),
+            nn.Conv2d(n_input_channels, 64, kernel_size=3, stride=1, padding=1),
             nn.ReLU(),
-            nn.Conv2d(32, 64, kernel_size=2, stride=1, padding=0),
+            nn.Conv2d(64, 64, kernel_size=3, stride=1, padding=1),
             nn.ReLU(),
-            nn.Conv2d(64, 128, kernel_size=2, stride=1, padding=0),
+            nn.Conv2d(64, 128, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(),
+            nn.Conv2d(128, 128, kernel_size=3, stride=1, padding=1),
             nn.ReLU(),
             nn.Flatten(),
         )
