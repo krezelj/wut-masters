8ed0735d8af3c723be383e66f769a312fdc35b47
Jasiek <jasiekk9@gmail.com>
2025-05-25 16:52:26 +0200

    Make exporting to onnx easier

--- onnx_export.py
+++ onnx_export.py
@@ -40,39 +40,51 @@ class ActorCriticNet(torch.nn.Module):
         return logits, value
     
 
-if __name__ == '__main__':
-    model = MaskablePPO.load('./models/othello_cnn_selfplay/model_49.zip', device="cpu")
-    # model = MaskablePPO.load('./train/othello_selfplay_cnn_small_skip_no_pool/.models/model_25.zip', device="cpu")
-    actor_critic = ActorCriticNet(model.policy)
-    actor = SingleNet(model.policy, mode="actor")
-    critic = SingleNet(model.policy, mode="critic")
-
-    example_input = torch.randn(1, 2, 8, 8)
+def export(model: MaskablePPO,
+           obs_shape: tuple,
+           unified: bool = False,
+           separate: bool = False):
     
-    torch.onnx.export(
-        actor_critic, 
-        example_input, 
-        "./models/actor_critic.onnx",
-        input_names=["input"], 
-        output_names=["logits", "value"], 
-        opset_version=11,
-        dynamic_axes={"input": {0: "batch_size"}, "logits": {0: "batch_size"}, "value": {0: "batch_size"}}
-    )
-    torch.onnx.export(
-        actor, 
-        example_input, 
-        "./models/actor.onnx",
-        input_names=["input"], 
-        output_names=["output"], 
-        opset_version=11,
-        dynamic_axes={"input": {0: "batch_size"}, "output": {0: "batch_size"}}
-    )
-    torch.onnx.export(
-        critic, 
-        example_input, 
-        "./models/critic.onnx",
-        input_names=["input"], 
-        output_names=["output"], 
-        opset_version=11,
-        dynamic_axes={"input": {0: "batch_size"}, "output": {0: "batch_size"}}
-    )
+    example_input = torch.randn(1, *obs_shape)
+    if unified:
+        actor_critic = ActorCriticNet(model.policy)
+        torch.onnx.export(
+            actor_critic, 
+            example_input, 
+            "./models/actor_critic.onnx",
+            input_names=["input"], 
+            output_names=["logits", "value"], 
+            opset_version=11,
+            dynamic_axes={"input": {0: "batch_size"}, "logits": {0: "batch_size"}, "value": {0: "batch_size"}}
+        )
+    
+    if separate:
+        actor = SingleNet(model.policy, mode="actor")
+        critic = SingleNet(model.policy, mode="critic")
+
+        torch.onnx.export(
+            actor, 
+            example_input, 
+            "./models/actor.onnx",
+            input_names=["input"], 
+            output_names=["output"], 
+            opset_version=11,
+            dynamic_axes={"input": {0: "batch_size"}, "output": {0: "batch_size"}}
+        )
+        torch.onnx.export(
+            critic, 
+            example_input, 
+            "./models/critic.onnx",
+            input_names=["input"], 
+            output_names=["output"], 
+            opset_version=11,
+            dynamic_axes={"input": {0: "batch_size"}, "output": {0: "batch_size"}}
+        )
+
+
+if __name__ == '__main__':
+    model = MaskablePPO.load('./train/othello_selfplay_cnn_batch/.models/model_130.zip', device="cpu")
+    # model = MaskablePPO.load('./models/othello_cnn_selfplay/model_72.zip', device="cpu")
+    # model = MaskablePPO.load('./train/othello_selfplay_cnn_small_skip_no_pool/.models/model_50.zip', device="cpu")
+    # model = MaskablePPO.load('./models/othello_mlp_selfplay/model_70.zip', device="cpu")
+    export(model, obs_shape=(2, 8, 8), separate=True)
